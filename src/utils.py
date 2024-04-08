79292c0cdb4b1d4ddc4f7ab8fdc336f7025dfcb5
AlexandrTikhonov <tihas75@mail.ru>
2024-04-08 23:09:44 +0300

    add def filtration_list(load_file):

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,9 @@ def getting_file(file_name):
     """Эта функция загружает и возвращает содержимое JSON-файла"""
     with open(file_name, "r") as load_file:
         return json.load(load_file)
+
+
+def filtration_list(load_file):
+    """Эта функция отсеивает пустые строки и оставляет значения ключа равное 'EXECUTED'"""
+    json_file = list(filter(lambda x: len(x) and x['state'] == 'EXECUTED', load_file))
+    return json_file
