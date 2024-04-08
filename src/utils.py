44755fe98cc88358bf40af096f9e9d97a9de6fbf
AlexandrTikhonov <tihas75@mail.ru>
2024-04-08 23:22:14 +0300

    add def sorted_date(json_file):

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 
 def getting_file(file_name):
@@ -11,3 +12,9 @@ def filtration_list(load_file):
     """Эта функция отсеивает пустые строки и оставляет значения ключа равное 'EXECUTED'"""
     json_file = list(filter(lambda x: len(x) and x['state'] == 'EXECUTED', load_file))
     return json_file
+
+
+def sorted_date(json_file):
+    """Эта функция сортирует список словарей в убывающем порядке дат, преобразованных в необходимый формат"""
+    json_sort = sorted(json_file,key=lambda x: datetime.strptime(x['date'],'%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
+    return json_sort
