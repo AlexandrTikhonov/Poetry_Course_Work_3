f1d8f6c4c36a9834271e6d0cc8dcef68d4df8fd7
AlexandrTikhonov <tihas75@mail.ru>
2024-04-09 04:37:13 +0300

    add test_sorted_date

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ operations = [
     {"id": 1, "state": "EXECUTED", "date": "2018-01-01T00:00:00.000000"},
     {"id": 2, "state": "EXECUTED", "date": "2018-02-01T00:00:00.000000"},
     {"id": 3, "state": "EXECUTED", "date": "2018-03-01T00:00:00.000000"},
-    {"id": 4, "state": "EXECUTED", "date": "2018-01-01T00:00:00.000000"}
+    {"id": 4, "state": "EXECUTED", "date": "2018-04-01T00:00:00.000000"}
 ]
 
 
@@ -27,6 +27,5 @@ def test_filtration_list(operations_fixture):
     assert len(filtration_list(operations_fixture)) == 4
 
 
-
-
-
+def test_sorted_date(operations_fixture):
+    assert [i["id"] for i in sorted_date(operations_fixture)] == [4, 3, 2, 1]
