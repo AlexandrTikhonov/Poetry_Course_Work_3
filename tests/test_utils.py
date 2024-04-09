b2c60aebd8f58e562dd656c8d34899460ac1c8c4
AlexandrTikhonov <tihas75@mail.ru>
2024-04-09 05:01:14 +0300

    add test_get_num

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -29,3 +29,12 @@ def test_filtration_list(operations_fixture):
 
 def test_sorted_date(operations_fixture):
     assert [i["id"] for i in sorted_date(operations_fixture)] == [4, 3, 2, 1]
+
+
+def test_get_date():
+    assert get_date("2018-01-01T00:00:00.000000") == "01.01.2018"
+
+
+def test_get_num():
+    assert get_num("Visa Platinum 1246377376343588") == "Visa Platinum 1246 37** **** 3588"
+    assert get_num("Счет 14211924144426031657") == "Счет **1657"
